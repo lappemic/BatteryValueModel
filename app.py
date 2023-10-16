cd4c919ad61fa5d4dff15239ff2532439cb8d054
Michael Lappert <michael.lappert@batterylog.ch>
2023-10-16 22:05:26 +0200

    doc: change some wording

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ remaining_value = model.remaining_value(original_price, soh, t, km, fastcharge_e
 # Display result
 battery_value = original_price*BATTERY_TO_EV_VALUE_RATIO
 battery_value_after_vat = battery_value*model.f_vat(VAT_RATE)
-st.title("Calculate the Remaining Value of your EV battery")
+st.title("Calculate easily the Remaining Value of your EV battery")
 st.write(f"The normalized remaining value of the battery with the given input parameters is {remaining_value:,.2f} from the cars original value.")
 st.write(f"The following diagram shows how much each feature contributes to the value depreciation of the batteries value")
 
